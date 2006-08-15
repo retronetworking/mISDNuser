2266eb66edba15c60790b4ed54ea2b47143763e4
Nadi Sarrar <nadi@beronet.com>
2006-08-15 16:29:13 +0000

    Moving the asn1 parse and encoding code from the mISDN sources to mISDNuser. suppserv.h specifies the interface. libsuppserv.so is the lib. So far, only one portion of CallDeflection is implemented.

---
+++ suppserv/asn1_basic_service.c
@@ -0,0 +1,14 @@
+/* $Id$
+ *
+ */
+
+#include "suppserv.h"
+
+// ======================================================================
+// Basic Service Elements EN 300 196-1 D.6
+
+int ParseBasicService(struct asn1_parm *pc, u_char *p, u_char *end, int *basicService)
+{
+	return ParseEnum(pc, p, end, basicService);
+}
+
